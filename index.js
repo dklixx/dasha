067731df121d2ea8494100d0162e75f5a9aba09b
dklixx <112929933+dklixx@users.noreply.github.com>
2022-10-21 11:44:04 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+function vik(){
+    let element = document.getElementById("but").classList.toggle('odna');
+}
+function kiv(){
+    let element = document.getElementById("bot").classList.add('dve');
+}
+function ivi(){
+    let element = document.getElementById("bat").classList.add('tri');
+}
+function joke(){
+ let ul1 = document.createElement('u1');
+document.body.append(ul);
+let joke = ('Кто рано встаёт, тому можно ещё поспать');
+let li = document.createElement('li');
+li.textContent = joke;
+ul.append(li);
+let joke1 = document.createTextNode('Деньги точно не зло — зло так быстро не исчезает.');
+let li1= document.createElement('li');
+li1.textContent = joke1;
+ul.append(li1);
+let joke2 = document.createTextNode('Каждый хочет услышать те самые заветные слова: "Я угощаю".');
+let li2 = document.createElement('li');
+li2.textContent = joke2;
+ul.append(li2);
+let joke3 = document.createTextNode('Тихие клиенты только в похоронном бюро.');
+let li3 = document.createElement('li');
+li3.textContent = joke3;
+ul.append(li3);
+}
